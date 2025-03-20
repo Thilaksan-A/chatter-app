3dc5ce9a05e302b715b7442e2504e5c448e15c89
Thilaksan Arulalan <z5162449@ad.edu.au>
2025-03-21 04:58:36 +1100

    adds routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './pages/login'
+import Register from './pages/register'
+import Welcome from './pages/Welcome'
+import { Toaster } from 'react-hot-toast'
+import { userStore } from './utils/user.state'
+import Dashboard from './pages/Dashboard'
 
 function App() {
-
+  const {user} = userStore();
   return (
-    <>
-      <div className='text-3xl text-amber-950'> 
-        Testing
-      </div>
+    <> 
+      <Toaster/>
+      <Routes>
+        <Route path= "/" element={user ? <Navigate  to={"/dashboard"} />  : <Welcome /> }  />
+        <Route path= "/dashboard" element={!user ? <Navigate  to={"/login"} /> : <Dashboard /> }  />
+        <Route path= "/login" element={user ? <Navigate  to={"/dashboard"} /> : <Login /> } />
+        <Route path= "/register" element={user ? <Navigate  to={"/dashboard"} /> : <Register />} />
+      </Routes>
     </>
   )
 }
