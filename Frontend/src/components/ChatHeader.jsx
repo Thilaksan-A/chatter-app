5b3fb646e53a6a9ec146f8cc75d35bf1e9fcb36d
Thilaksan Arulalan <z5162449@ad.edu.au>
2025-03-22 04:21:09 +1100

    adds chatheader for chat window

---
+++ Frontend/src/components/ChatHeader.jsx
@@ -0,0 +1,24 @@
+import profileImage from "../assets/profile.webp";
+import { chatStore } from "../utils/chat.state";
+
+const ChatHeader = (user) => {
+  const {setSelectedUser} = chatStore();
+  return (
+    <div className="flex justify-between items-center bg-gray-900 p-4 text-white">
+      <div className="flex items-center">
+        <img src={profileImage} alt={user.fullName} className="w-10 h-10 rounded-full"/> 
+        <div>
+          <h3 className="text-xl">{user.fullName}</h3>
+        </div>
+      </div>
+      <button className="text-gray-400 cursor-pointer"  onClick={() => {
+        setSelectedUser(null);
+      }}>
+         <X />
+      </button>
+    </div>
+  );
+}
+
+
+export default ChatHeader;
