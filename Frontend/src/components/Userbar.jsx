147b0c130de9a8d33f868bc903751abbe2c2a8ae
Thilaksan Arulalan <z5162449@ad.edu.au>
2025-03-22 04:16:34 +1100

    adds side userbar for chat window

---
+++ Frontend/src/components/Userbar.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { chatStore } from "../utils/chat.state.js";
+import { Users } from 'lucide-react';
+import profileImage from "../assets/profile.webp";
+
+
+export const Userbar = () => {
+  const { users, setSelectedUser, selectedUser, getUsers } = chatStore();
+  
+  useEffect(() => {
+    getUsers()
+  }, [getUsers]);
+
+  return(
+    <div className="w-72 p-5 border-r">
+      <div className="flex items-center gap-2">
+        <Users className="text-xl" />
+        <span className="font-medium">Contacts</span>
+      </div>
+      <div className="mt-4">
+        {users && users.length > 0 && users.map((user) => (
+          <button
+            key={user._id}
+            onClick={() => setSelectedUser(user)}
+            className={`flex items-center gap-3 p-3 w-full ${selectedUser?._id === user._id ? "bg-gray-200" : ""}`}
+          >
+            <img
+              src={profileImage}
+              alt={user.name}
+              className="w-10 h-10 rounded-full"
+            />
+            <div>{user.fullName}</div>
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+      
+}
+
+
+export default Userbar;
