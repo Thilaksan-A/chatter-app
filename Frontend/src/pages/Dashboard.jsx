d53ab7024292188102882870a1a0509a4a3960ce
Thilaksan Arulalan <z5162449@ad.edu.au>
2025-03-22 04:17:47 +1100

    adds userbar and chatwindow section to homepage

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,20 @@
+import ChatWindow from "../components/ChatWindow";
 import Header from "../components/Header"
+import Userbar from "../components/Userbar";
 
 const Dashboard = () => {
-    return (
-        <Header />
-    )
+  return (
+    <>  
+      <Header />
+      <div className="pt-44 flex h-screen">
+        <Userbar />
+        <ChatWindow />
+        
+      </div>
+     
+    </>
+      
+  )
 }
 
 export default Dashboard;
